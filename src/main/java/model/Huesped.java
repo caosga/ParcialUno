74ed6ef6e3ae5dfabef07cedcd5004d2e99708fe
Daniel <ldocampoc@uqvirtual.edu.co>
2026-09-24 23:31:17 -0500

    Atributos

--- src/main/java/model/Huesped.java
+++ src/main/java/model/Huesped.java
@@ -5,3 +5,50 @@ public class Huesped {
     public String nombreCompleto, documentoIdentidad, telefono, paisProcedencia;
     private String correoElectronico;
 }
+public Huesped(String nombreCompleto, String documentoIdentidad, long telefono, String correoElectronico, String paisProcedencia) {
+    this.nombreCompleto = nombreCompleto;
+    this.documentoIdentidad = documentoIdentidad;
+    this.telefono = telefono;
+    this.correoElectronico = correoElectronico;
+    this.paisProcedencia = paisProcedencia;
+}
+public String getNombreCompleto() {
+    return nombreCompleto;
+}
+
+public void setNombreCompleto(String nombreCompleto) {
+    this.nombreCompleto = nombreCompleto;
+}
+
+public String getDocumentoIdentidad() {
+    return documentoIdentidad;
+}
+
+public void setDocumentoIdentidad(String documentoIdentidad) {
+    this.documentoIdentidad = documentoIdentidad;
+}
+
+public long getTelefono() {
+    return telefono;
+}
+
+public void setTelefono(long telefono) {
+    this.telefono = telefono;
+}
+
+public String getCorreoElectronico() {
+    return correoElectronico;
+}
+
+public void setCorreoElectronico(String correoElectronico) {
+    this.correoElectronico = correoElectronico;
+}
+
+public String getPaisProcedencia() {
+    return paisProcedencia;
+}
+
+public void setPaisProcedencia(String paisProcedencia) {
+    this.paisProcedencia = paisProcedencia;
+}
+}
